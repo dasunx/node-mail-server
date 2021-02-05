309d90983dbf5f39d803e623084faeaf006beed2
dasunx <mwdasun@gmail.com>
2021-02-05 12:28:26 +0530

    Contact form

--- routes/invitation-routes.js
+++ routes/invitation-routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { contact } = require('../controllers/contact-controller');
 const { sendInvitation } = require('../controllers/invitation-controller');
 const router = express.Router();
 
 router.post('/', sendInvitation);
-
+router.post('/contact', contact);
 module.exports = router;
